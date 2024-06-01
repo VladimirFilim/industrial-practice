a1effbc435fe81272e0750ef22b6bbdda2abf1fc
VladimirFilim <boterfly9@gmail.com>
2024-06-02 00:21:41 +0300

    Карточки появляются рубашкой вверх, клики работают

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function createTable() {
     for (let i = 0; i < numRows; i++) {
         tableHTML += '<tr>';
         for (let j = 0; j < numCols; j++) {
-            tableHTML += `<td align="center" style="width: 125px;"><img id=${imageIndex} onClick="revealCard()" width="125" height="125"></td>`;
+            tableHTML += `<td align="center" style="width: 125px;"><img id=${imageIndex} src="${backImage}" onClick="revealCard()" width="125" height="125"></td>`; // Установить изображение рубашки
             imageIndex++;
         }
         tableHTML += '</tr>';
@@ -251,5 +251,6 @@ function clearTable() {
 
 function changeTheme(newTheme) {
     theme = newTheme;
+    backImage = `img/${theme}/back.jpg`;
     startNewGame();
 }
